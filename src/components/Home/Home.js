3ab68ca5ed7eba86473008b85f3a44e696680cb6
alan <alanarostegui@hotmail.com>
2020-05-31 19:55:43 -0500

    feature(home & header component): render and show logos

---
+++ src/components/Home/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import "./Home.css";
+
+const initialState = {};
+
+class Home extends Component {
+  render() {
+    return <div>Home</div>;
+  }
+}
+
+export default Home;
