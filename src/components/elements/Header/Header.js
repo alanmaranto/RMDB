df1179529af669387b9ac7eac57e27c949398abb
alan <alanarostegui@hotmail.com>
2020-06-15 22:30:22 -0500

    feature(react router): React Router, Links to movie id

--- src/components/elements/Header/Header.js
+++ src/components/elements/Header/Header.js
@@ -1,12 +1,23 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "./Header.css";
 
 const Header = () => {
   return (
     <div className="rmdb-header">
       <div className="rmdb-header-content">
-        <img src="./images/reactMovie_logo.png" alt="rmdb-logo" className="rmdb-logo" />
-        <img src="./images/tmdb_logo.png" alt="tmdb-logo" className="rmdb-tmdb-logo"/>
+        <Link to="/">
+          <img
+            src="./images/reactMovie_logo.png"
+            alt="rmdb-logo"
+            className="rmdb-logo"
+          />
+        </Link>
+        <img
+          src="./images/tmdb_logo.png"
+          alt="tmdb-logo"
+          className="rmdb-tmdb-logo"
+        />
       </div>
     </div>
   );
