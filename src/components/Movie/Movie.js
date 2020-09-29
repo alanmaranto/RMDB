c8d939e487073e8d72e831df9abcdadb327f96d4
alan <alanarostegui@hotmail.com>
2020-09-28 22:35:43 -0500

    refactor(movie): destructuring

--- src/components/Movie/Movie.js
+++ src/components/Movie/Movie.js
@@ -24,7 +24,7 @@ class Movie extends Component {
     } else {
       this.setState({ loading: true });
       // Fetch the movie
-      const endpoint = `${API_URL}movie/${this.props.match.params.movieId}?api_key=${API_KEY}&language=es-ES`;
+      const endpoint = `${API_URL}movie/${movieId}?api_key=${API_KEY}&language=es-ES`;
       this.fetchItems(endpoint);
     }
   }
